bf1d013b419d7194001542b656da77d1683c5e67
K1nGsmaN-hub <15162342h@gmail.com>
2021-05-19 22:38:21 +0300

    add Container in app/sc

---
+++ app/sc/Container.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1200px;
+  width: 100%;
+
+  padding: 0 10px;
+  margin: 0 auto;
+
+  box-sizing: border-box;
+`;
