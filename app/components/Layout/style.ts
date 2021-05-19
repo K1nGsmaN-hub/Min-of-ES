c15834eac6e809ba49aa98dcc46bf8886d0cb37e
K1nGsmaN-hub <15162342h@gmail.com>
2021-05-19 23:53:28 +0300

    edit Layout component in app/

--- app/components/Layout/style.ts
+++ app/components/Layout/style.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-export const LayoutWrapper = styled.div`
-  body {
-    font-family: "Arial", sans-serif;
-    min-height: 100vh;
-  }
+export const LayoutWrapper = styled.main`
+  font-family: "Arial", sans-serif;
+  min-height: 100vh;
+  
+  padding-bottom: 60px;
 `;
